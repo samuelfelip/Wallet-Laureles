de44b90ea3001a88ae12c85a095f816efef50c62
Sam Herrera <37428515+samuelfelip@users.noreply.github.com>
2024-09-18 17:19:25 -0500

    Commit 5 Pm Cambios estructurales

--- src/CleanArchitecture/Infrastructure/Data/ApplicationDbContext.cs
+++ src/CleanArchitecture/Infrastructure/Data/ApplicationDbContext.cs
@@ -1,11 +1,15 @@
 using System.Reflection;
 using CleanArchitecture.Application.Common;
+using Humanizer.Configuration;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
 
 namespace CleanArchitecture.Infrastructure.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
 {
+    public IConfiguration Configuration { get; }
     public DbSet<Book> Books { get; set; }
     public DbSet<User> Users { get; set; }
 
@@ -16,7 +20,11 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
     }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        
-        optionsBuilder.UseNpgsql();
+        optionsBuilder.UseNpgsql(Configuration.GetConnectionString("WebApiDatabase"));
     }
 }
+/*
+ ///para tener 
+ ///en cuenta migracion en postgres!!!!
+ Scaffold-DbContext “Host=localhost;Database=CryptoMoney;Username=root;Password=L4M4sS3gvr4$” Npgsql.EntityFrameworkCore.PostgreSQL -OutputDir Models
+ */
